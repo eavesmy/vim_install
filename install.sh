b68c4eaead440b585c7def04a8116cd4732aa2bd
eavesmy <eavesmy@gmail.com>
2019-12-08 14:06:36 +0800

    up

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 #cp vimrc ~/.vimrc
 
-#git clone https://github.com/dracula/vim.git ~/.vim
-#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-#git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
+git clone https://github.com/dracula/vim.git ~/.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
 git clone https://github.com/Shutnik/jshint2.vim.git ~/.vim/bundle/jshint2.vim
 
 cp ./jshintrc ~/.jshintrc
